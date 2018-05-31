566471bb7141a32629708b0d34d26c599aa185c2
Gabrielle Saman <gabi.duarte@gmail.com>
2018-05-31 18:07:59 -0300

    Fix search logic to include description from state

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -7,12 +7,13 @@ export const changeDescription = event => ({
     payload: event.target.value
 });
 
-export const search = () => {
-    const request = axios.get(`${URL}?sort=-createdAt`)
+export const search = (description) => {
 
-    return {
-        type: 'TODO_SEARCHED',
-        payload: request
+    return (dispatch, getState) => {
+        const description = getState().todo.description;
+        const search = description ? `&description__regex=/${description}/` : ''
+        const request = axios.get(`${URL}?sort=-createdAt${search}`)
+            .then(res => dispatch({ type: 'TODO_SEARCHED', payload: res.data}))
     }
 }
 
@@ -44,7 +45,7 @@ export const remove = (todo) => {
 }
 
 export const clear = () => {
-    return {
+    return [{
         type: 'SEARCH_CLEARED'
-    }
+    }, search()]
 }
